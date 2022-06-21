c352c0e621db60005e659f25fd08f9c72ed64304
Pedro Henrique <pedrinfnn@gmail.com>
2022-06-20 22:01:59 -0300

    class 12

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/app_controller.dart';
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.red), home: HomePage());
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (BuildContext context, Widget? child) {
+          return MaterialApp(
+              theme: ThemeData(
+                  primarySwatch: AppController.instance.isDartTheme
+                      ? Colors.red
+                      : Colors.red,
+                  brightness: AppController.instance.isDartTheme
+                      ? Brightness.dark
+                      : Brightness.light),
+              home: HomePage());
+        });
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,27 +10,23 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor:
+            AppController.instance.isDartTheme ? Colors.black26 : Colors.red,
         title: Text('Pedro Dev <3'),
       ),
       body: Center(
-        child: Container(
-          height: 50,
-          width: 50,
-          color: Colors.black,
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
-              height: 20,
-              width: 20,
-              color: Colors.green,
-            ),
-          ),
-        ),
+        child: Switch(
+            value: AppController.instance.isDartTheme,
+            onChanged: (value) {
+              setState(() {
+                AppController.instance.changeTheme();
+              });
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
