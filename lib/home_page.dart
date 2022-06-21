014ae03e26247df324511a9a1f2c1f91df72a07d
Pedro Henrique <pedrinfnn@gmail.com>
2022-06-20 22:06:31 -0300

    class 13

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,19 +15,12 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [CustomSwitcher()],
         backgroundColor:
             AppController.instance.isDartTheme ? Colors.black26 : Colors.red,
         title: Text('Pedro Dev <3'),
       ),
-      body: Center(
-        child: Switch(
-            value: AppController.instance.isDartTheme,
-            onChanged: (value) {
-              setState(() {
-                AppController.instance.changeTheme();
-              });
-            }),
-      ),
+      body: Center(child: Text('Like a dark mode <3')),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -37,5 +30,18 @@ class HomePageState extends State<HomePage> {
         },
       ),
     );
+  }
+}
+
+class CustomSwitcher extends StatelessWidget {
+  const CustomSwitcher({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+        value: AppController.instance.isDartTheme,
+        onChanged: (value) {
+          AppController.instance.changeTheme();
+        });
   }
 }
