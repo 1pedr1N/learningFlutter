ffea9ea61e08769d2e08d99a87d0beda612dab30
Pedro Henrique <pedrinfnn@gmail.com>
2022-06-22 12:28:31 -0300

    some funny changes

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/app_controller.dart';
+import 'package:flutter_learning/home_page.dart';
 import 'login_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -12,14 +13,19 @@ class AppWidget extends StatelessWidget {
         animation: AppController.instance,
         builder: (BuildContext context, Widget? child) {
           return MaterialApp(
-              theme: ThemeData(
-                  primarySwatch: AppController.instance.isDartTheme
-                      ? Colors.red
-                      : Colors.red,
-                  brightness: AppController.instance.isDartTheme
-                      ? Brightness.dark
-                      : Brightness.light),
-              home: LoginPage());
+            theme: ThemeData(
+                primarySwatch: AppController.instance.isDartTheme
+                    ? Colors.red
+                    : Colors.red,
+                brightness: AppController.instance.isDartTheme
+                    ? Brightness.dark
+                    : Brightness.light),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => LoginPage(),
+              '/home': (context) => HomePage()
+            },
+          );
         });
   }
 }
